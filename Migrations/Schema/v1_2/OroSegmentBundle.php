98bc7fa72ea3a2922d1f200462ad1d27732ca3ef
nikolay <nikolay@nikolays-MacBook-Pro.local>
2014-07-28 11:56:14 +0300

    OEE-41: Add organization relation to each ACL protected entity and migrations

---
+++ Migrations/Schema/v1_2/OroSegmentBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Oro\Bundle\SegmentBundle\Migrations\Schema\v1_2;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroSegmentBundle implements Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('oro_segment');
+        $table->addColumn('organization_id', 'integer', ['notnull' => false]);
+        $table->addIndex(['organization_id'], 'IDX_D02603B332C8A3DE', []);
+        $table->addForeignKeyConstraint($schema->getTable('oro_organization'), ['organization_id'],
+            ['id'], ['onDelete' => 'SET NULL', 'onUpdate' => null]
+        );
+    }
+}
